6dd70d8f1e34b6eab6231880a7bd0e2e2aacc058
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-03-09 09:23:15 -0500

    add comments

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const panels = document.querySelectorAll('.panel')
+console.log(panels.length) //select all the cards with panels;
 
-panels.forEach(panel => {
+panels.forEach((panel) => {
     panel.addEventListener('click', () => {
+        console.log('test click');
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
+    panels.forEach((panel) => {
+        console.log(panel.classList);
         panel.classList.remove('active')
     })
 }
