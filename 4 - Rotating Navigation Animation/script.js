41fd4908aa99c9eb663870f91c7a6af6561be15d
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-03-14 09:38:36 -0400

    5 - Hidden Search Widget

--- 4 - Rotating Navigation Animation/script.js
+++ 4 - Rotating Navigation Animation/script.js
@@ -1,7 +1,9 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
+//either for class (query  selector) or can be selected by id getelementbyid
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
+//when click happends, we'll add or remove class to the container
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
