b305b9afd6748489fbdec2b0deee8739bbe4d4b8
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-03-15 08:27:18 -0400

    5 - Hidden Search Widget

--- 5 - Hidden Search Widget/script.js
+++ 5 - Hidden Search Widget/script.js
@@ -2,6 +2,9 @@ const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
+//event listener on button ( click 
+//+ action toggle class "active" ( add/ remove))
+//+ action force focus on search input
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
