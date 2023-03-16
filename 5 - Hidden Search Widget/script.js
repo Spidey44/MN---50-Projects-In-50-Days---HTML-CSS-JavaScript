04a5d027c398abebec3c1a6bc92343086101228e
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-03-16 09:00:18 -0400

    Update 5

--- 5 - Hidden Search Widget/script.js
+++ 5 - Hidden Search Widget/script.js
@@ -1,3 +1,4 @@
+//target
 const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
@@ -5,7 +6,11 @@ const input = document.querySelector('.input')
 //event listener on button ( click 
 //+ action toggle class "active" ( add/ remove))
 //+ action force focus on search input
+//$(selector).on('eventName', function() => run this code when the event happens
 btn.addEventListener('click', () => {
+    //Mouse Events, Touch Events,Keyboard Events...
     search.classList.toggle('active')
+    //The classList property is read-only
+    //Toggle "active" on and off:
     input.focus()
 })
