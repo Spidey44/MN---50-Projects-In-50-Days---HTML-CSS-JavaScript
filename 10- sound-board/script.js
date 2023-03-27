d4fb289481e9ea5f37884ab12057504ef84c0854
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-03-27 09:20:23 -0400

    10- sound-board

---
+++ 10- sound-board/script.js
@@ -0,0 +1,35 @@
+
+// gather all variable in a array
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+// Loop through each and create a button element
+//add a class btn to the boutton for styling
+// set innerText to sound
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    console.log(btn)
+    console.log(btn.innerText)
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        //js API  are function /method for audio element to stopSongs() or play()
+        //avoid playing together
+        stopSongs()
+
+        //select audiotag to play baased on id
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        //select the previous song nd stop with pause() and reset to 0
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
