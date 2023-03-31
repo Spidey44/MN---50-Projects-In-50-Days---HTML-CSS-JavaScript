e06ccf72922a68f81ccbafe3ad72add0f1be9731
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-03-31 09:10:07 -0400

    12 - event-keycodes

---
+++ 12 - event-keycodes/script.js
@@ -0,0 +1,32 @@
+window.addEventListener('keydown', (e) => {
+  console.log(e)
+  console.log(e.code)
+  console.log(e.key)
+  console.log(e.KeyCode)
+  //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+  console.log(e.code)
+}
+)
+
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (event) => {
+  //template literal
+  insert.innerHTML = `
+  <div class="key">
+  //if this... then/? ...else/:...
+  ${event.key === ' ' ? 'Space' : event.key} 
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
+</div>
+  `
+})
