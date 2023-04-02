0faa669263524d0d3d67a104298db4f92c2f1554
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-04-02 14:57:41 -0400

    #13 continue

--- 13 - faq-collapse/script.js
+++ 13 - faq-collapse/script.js
@@ -1,7 +1,13 @@
+// 1- bring the toggle button wit Query elector create list
+// 2- Loop through nodelist(forEach)
+// 3- ass click with eventlistnener
+// 4- toggle the active class on the parent node parentNode & classList.toggle()
+
 const toggles = document.querySelectorAll('.faq-toggle')
 
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
+        //toggle method makes it easier to add and remove
         toggle.parentNode.classList.toggle('active')
     })
 })
