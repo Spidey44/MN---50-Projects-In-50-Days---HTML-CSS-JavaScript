4a92fce338ec337a1e32e702bb65a33ae47e7d4b
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-04-10 09:50:00 -0400

    Complete 17

--- 17 - drink-water/script.js
+++ 17 - drink-water/script.js
@@ -5,17 +5,24 @@ const remained = document.getElementById('remained')
 
 updateBigCup()
 
+//loop all through small cups
 smallCups.forEach((cup, idx) => {
+    console.log('8 cups from 0 to 7' + idx)
     cup.addEventListener('click', () => highlightCups(idx))
 })
 
 function highlightCups(idx) {
-    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+    //if clicked contais full + next does not contain full decrement current index clicked
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
         idx--
     }
 
     smallCups.forEach((cup, idx2) => {
-        if(idx2 <= idx) {
+        //index in the loop to fill all the cup in order
+        console.log('index clocked on' + idx)
+        if (idx2 <= idx) {
+            //if cup before the one clicked empty, make it + all previous full
+            //if i click on #4 and #3 empty make all 1 to 4 full
             cup.classList.add('full')
         } else {
             cup.classList.remove('full')
@@ -26,22 +33,30 @@ function highlightCups(idx) {
 }
 
 function updateBigCup() {
+    //select all full cups 
     const fullCups = document.querySelectorAll('.cup-small.full').length
+    console.log(fullCups)
+    //8 cups
     const totalCups = smallCups.length
+    console.log(fullCups)
 
-    if(fullCups === 0) {
+    //Hide hidden if empty or 0%
+    if (fullCups === 0) {
         percentage.style.visibility = 'hidden'
         percentage.style.height = 0
+        //filling cup Show the %based on the 330 height of large cup 
     } else {
         percentage.style.visibility = 'visible'
         percentage.style.height = `${fullCups / totalCups * 330}px`
+        //display the text 
         percentage.innerText = `${fullCups / totalCups * 100}%`
     }
-
-    if(fullCups === totalCups) {
+    //remove the remainded at the top ONLY if cup 100% full
+    if (fullCups === totalCups) {
         remained.style.visibility = 'hidden'
         remained.style.height = 0
     } else {
+        //show the number of cup remained from 2 liters
         remained.style.visibility = 'visible'
         listers.innerText = `${2 - (250 * fullCups / 1000)}L`
     }
