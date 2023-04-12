1b9c6744dc56629c5950873adb7fbca113ce8fae
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-04-12 09:54:37 -0400

    Project 18 movie app

---
+++ 18 - movie-app/script.js
@@ -0,0 +1,79 @@
+//https://www.themoviedb.org/documentation/api
+// endpoints https://www.themoviedb.org/documentation/api/discover
+//https://www.themoviedb.org/settings/api
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2899a1747cce1d81caa27b9ed1789627&page=1'
+//test API url on browser to see all the results
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+//URL before using image as per documentation - https://developers.themoviedb.org/3/getting-started/images
+//p/w1280 being the size
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=2899a1747cce1d81caa27b9ed1789627c&query="'
+//concatenate search to query
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+    //get the data
+    const res = await fetch(url)
+    //translate in JSON
+    const data = await res.json()
+    //array of 20 objcts
+    console.log(data.results)
+
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    //clear the mean
+    main.innerHTML = ''
+    //loop through data fetched
+    movies.forEach((movie) => {
+        //destucturing
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}s
+        </div>
+        `
+        //add this into the DOM. append the movie element
+        main.appendChild(movieEl)
+    })
+}
+//pass the vote rating into the functin to determine colour
+//result go to class="${getClassByRate(vote_average)}"
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+    //if search term exist and not equal to anything
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+        //clear the search
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
