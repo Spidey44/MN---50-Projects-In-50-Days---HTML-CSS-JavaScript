bf3db16acb187748825fca43b042dc7f7b7d95c2
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-04-15 23:11:48 -0400

    final #19

--- 19- background-slider/script.js
+++ 19- background-slider/script.js
@@ -7,25 +7,27 @@ const rightBtn = document.getElementById('right')
 let activeSlide = 0
 
 rightBtn.addEventListener('click', () => {
+  // as i change setBgToBody(), increment activeSlide ( middle image)
   activeSlide++
 
   //active list retruns to 0, starts over after last
   if (activeSlide > slides.length - 1) {
     activeSlide = 0
   }
-
+  // initial when set to 0
   setBgToBody()
   setActiveSlide()
 })
 
 leftBtn.addEventListener('click', () => {
+  // as i change setBgToBody(), decrement activeSlide ( middle image)
   activeSlide--
 
   //active list retruns to last, starts over before first
   if (activeSlide < 0) {
     activeSlide = slides.length - 1
   }
-
+  // initial when set to 0
   setBgToBody()
   setActiveSlide()
 })
@@ -34,11 +36,13 @@ setBgToBody()
 
 //when called, it adds background image css on node list
 function setBgToBody() {
+  // slides node list[index] start at 0
   body.style.backgroundImage = slides[activeSlide].style.backgroundImage
 }
 
 function setActiveSlide() {
+  // loop through all the list and remove
   slides.forEach((slide) => slide.classList.remove('active'))
-
+  // then add slide in active
   slides[activeSlide].classList.add('active')
 }
