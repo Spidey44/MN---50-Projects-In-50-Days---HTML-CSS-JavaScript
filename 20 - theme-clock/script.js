944f1bcccb5294eec6647bf9e9f2bc703c81acd5
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-04-17 08:53:07 -0400

    Project#20 completed

--- 20 - theme-clock/script.js
+++ 20 - theme-clock/script.js
@@ -8,7 +8,7 @@ const toggle = document.querySelector('.toggle')
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-// dark mode or light mode
+// dark mode or light mode based on primary and secondary color
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
     if (html.classList.contains('dark')) {
@@ -22,31 +22,36 @@ toggle.addEventListener('click', (e) => {
 
 function setTime() {
     const time = new Date();
-    console.log(time)
+    console.log('time ' + time)
     const month = time.getMonth()
-    console.log(month)
+    console.log('month ' + month)
     const day = time.getDay()
-    console.log(day)
+    console.log('day ' + day)
     const date = time.getDate()
-    console.log(date)
+    console.log('date ' + date)
     const hours = time.getHours()
-    console.log(hours)
+    console.log('hour ' + hours)
     // 12 hours clock
     const hoursForClock = hours % 12
     const minutes = time.getMinutes()
-    console.log(minutes)
+    console.log('min ' + minutes)
     const seconds = time.getSeconds()
-    console.log(seconds)
+    console.log('sec ' + seconds)
     // set PM or AM based on hour
     const ampm = hours >= 12 ? 'PM' : 'AM'
 
-    // map hour, min or sec to 360 degree
+    // change needle with map hour, min or sec to 360 degree
+    //example map hours constant 0-11 from 0 to 360% position
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    //example map min const 0-59 from 0 to 360% position
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
-    // add 0 to number is less than 10
+
+    //Add to HTML
+    // option 12 or 24h format 
+    // 12h format -> ternary add 0 if number is less than 10 
     timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
-    //dat and mont from index of array
+    //dat and monh comes from index of array cretated above
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 // map hour, min or sec t0 360
@@ -55,8 +60,8 @@ function setTime() {
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
+//keep calling the function
 setTime()
 
-// function called every sec or 1k mil sec
+// function called every sec or 1000 mil sec
 setInterval(setTime, 1000)
