0c9c0189c36963557ca18d171fbfde70ee3b5b8e
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-04-27 09:20:11 -0400

    Complete #26

--- 25 - content-placeholder/script.js
+++ 25 - content-placeholder/script.js
@@ -7,7 +7,7 @@ const name = document.getElementById('name')
 const date = document.getElementById('date')
 
 // all animated classes
-// node list
+// node list of severa classes
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
@@ -15,6 +15,7 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 setTimeout(getData, 2500)
 
 // hard coded or come from API
+// data to bring
 function getData() {
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
@@ -26,7 +27,7 @@ function getData() {
   name.innerHTML = 'John Doe'
   date.innerHTML = 'Oct 08, 2020'
 
-  // go through node list  and remove the class
+  // go through node list and remove the class
   animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
   animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
 }
