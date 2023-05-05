65ab44c27131b718e572789bb0bb417735d8cd80
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-05 09:00:40 -0400

    30 completed

--- 30 - double-click-heart/script.js
+++ 30 - double-click-heart/script.js
@@ -7,27 +7,32 @@ let clickTime = 0
 let timesClicked = 0
 
 // click event listener on loveMe class
+// easier is event double click ->" bdclick"
 loveMe.addEventListener('click', (e) => {
-    console.log(123)
+    console.log("clicked", 123)
+    // test time between click. if short release heart
     if (clickTime === 0) {
         clickTime = new Date().getTime()
-        console.log('value of click #1', clickTime)
+        // works only once inside if statement cox no longer equal to 0
+        console.log('value of initial click #1', clickTime)
     } else {
-        // compare 2 click time
+        // if 2nd click, not equal to 0, compare 2 click time
         // considered double click if time between less than 800
         if ((new Date().getTime() - clickTime) < 800) {
-            console.log(234)
+            console.log('value of click #2 short wait', clickTime)
             // if less, call createHEart function
             createHeart(e)
             // reset if more
             clickTime = 0
+            console.log('reset click #2 short wait', clickTime)
         } else {
             clickTime = new Date().getTime()
+            console.log('value of click #3 long wait ', clickTime)
         }
     }
 })
 
-// create heart icon
+// create heart icon with arrow function
 const createHeart = (e) => {
     const heart = document.createElement('i')
     heart.classList.add('fas')
@@ -48,16 +53,16 @@ const createHeart = (e) => {
     const yInside = y - topOffset
     console.log("position within image is ", xInside, yInside)
 
-    // Set up the hart property position withing the image
+    // Set up the hart property position relative within the image
     heart.style.top = `${yInside}px`
     heart.style.left = `${xInside}px`
 
     // insert the heart created
     loveMe.appendChild(heart)
 
-    // increment the time click displayed counter
+    // increment the time by 1 click displayed counter
     times.innerHTML = ++timesClicked
 
-    // remove the inserted icon from the DOM
+    // remove the inserted icon from the DOM. Otherwise accumulate after disappear
     setTimeout(() => heart.remove(), 1000)
 }
