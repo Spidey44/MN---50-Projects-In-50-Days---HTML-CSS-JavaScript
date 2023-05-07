6665fc5354d95d9883bc7b625e4b0539823a1641
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-07 07:37:19 -0400

    end project 31

--- 31 - auto-text-effect/script.js
+++ 31 - auto-text-effect/script.js
@@ -1,25 +1,29 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
+// text typed out
 const text = 'We Love Programming!'
-// index variable initialized to 1
+// Where are we in the text. index variable initialized to 1
 let idx = 1
+// 1000 speed is one sec
 let speed = 300 / speedEl.value
 
 writeText()
 
 function writeText() {
-    // split the text into letter W (from 0 to 1)
+    // split the text into letter W (cut from 0 to 1)
+    // slice takes the range of the text (start, end)
     textEl.innerText = text.slice(0, idx)
 
     idx++
     // if reach the end, reset index to 1, back to fist letter
     if (idx > text.length) {
+        // if true start over
         idx = 1
     }
     // wait time (speed) before calling new letter
     setTimeout(writeText, speed)
 }
 
-// event listen to speed input ( type or arrow key)
-//
+// event listen to number (speed) input ( type or arrow key)
+
 speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
