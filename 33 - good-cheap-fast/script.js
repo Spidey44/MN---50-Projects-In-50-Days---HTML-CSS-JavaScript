8c5260938161765a4bed86b1450ca5fdebcb20a4
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-10 08:39:15 -0400

    Project 33

---
+++ 33 - good-cheap-fast/script.js
@@ -0,0 +1,28 @@
+// Grab all toggles + the ones by ID
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+// event listener on each toggle -> on change call function do the trick passing element clicked
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickedOne) {
+    // if all checked clicked uncheck one
+    if (good.checked && cheap.checked && fast.checked) {
+        if (good === theClickedOne) {
+            // uncheck fast for good clicked
+            fast.checked = false
+        }
+
+        if (cheap === theClickedOne) {
+            // uncheck good for cheap clicked
+            good.checked = false
+        }
+
+        if (fast === theClickedOne) {
+            // uncheck cheap for fast clicked
+            cheap.checked = false
+        }
+    }
+}
