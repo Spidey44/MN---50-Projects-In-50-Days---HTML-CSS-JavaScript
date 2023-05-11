5959f7b5e2cabd93ab174ba846d9341fd8f672c9
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-11 06:21:36 -0400

    33ended

--- 33 - good-cheap-fast/script.js
+++ 33 - good-cheap-fast/script.js
@@ -6,23 +6,25 @@ const fast = document.querySelector('#fast')
 
 // event listener on each toggle -> on change call function do the trick passing element clicked
 toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
-
 function doTheTrick(theClickedOne) {
     // if all checked clicked uncheck one
     if (good.checked && cheap.checked && fast.checked) {
         if (good === theClickedOne) {
             // uncheck fast for good clicked
             fast.checked = false
+            console.log("test", e.target)
         }
 
         if (cheap === theClickedOne) {
             // uncheck good for cheap clicked
             good.checked = false
+            console.log("test", e.target)
         }
 
         if (fast === theClickedOne) {
             // uncheck cheap for fast clicked
             cheap.checked = false
+            console.log("test", e)
         }
     }
 }
