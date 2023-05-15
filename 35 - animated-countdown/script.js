ec1c17219bad9adb580d434e822ceeba24394a7f
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-15 09:09:39 -0400

    35 ended

--- 35 - animated-countdown/script.js
+++ 35 - animated-countdown/script.js
@@ -1,34 +1,41 @@
+// node list of all the numbers between span
 const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
 
 runAnimation()
-
+// reset dom to initial state
 function resetDOM() {
+  // 1/4 reset dom to initial state
   counter.classList.remove('hide')
+  // 2/4 reset dom to initial state
   finalMessage.classList.remove('show')
 
-  // clear the value for each num
+  // 3/4 clear the value for each num
   nums.forEach((num) => {
     num.classList.value = ''
   })
 
-  // add class in to the first span at index 0
+  // 4/4 add class in to the first span at index 0
   nums[0].classList.add('in')
 }
 
 function runAnimation() {
+  // loop through node list -> num each individual item found + idx for each iteratioin starting at 0
   nums.forEach((num, idx) => {
+    console.log("span element with num values", num)
     console.log("index values", idx)
-    console.log("num values", num)
+    // total -1 to identify the last num
     const nextToLast = nums.length - 1
 
     // function fired when animation ended
     num.addEventListener('animationend', (e) => {
+      // if in animation goIn + has reach last num
       if (e.animationName === 'goIn' && idx !== nextToLast) {
         num.classList.remove('in')
         num.classList.add('out')
+        // if animation go Out + has number coming next. Sibling = same level element
       } else if (e.animationName === 'goOut' && num.nextElementSibling) {
         num.nextElementSibling.classList.add('in')
       } else {
@@ -38,7 +45,7 @@ function runAnimation() {
     })
   })
 }
-
+// ehen click replay button reset DOM and run Animation
 replay.addEventListener('click', () => {
   resetDOM()
   runAnimation()
