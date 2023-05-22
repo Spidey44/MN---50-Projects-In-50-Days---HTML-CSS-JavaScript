e98b3b774bb4785869a645821c5b6cb8c25ef319
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-22 10:26:09 -0400

    ended39

--- 39 - mobile-tab-navigation/script.js
+++ 39 - mobile-tab-navigation/script.js
@@ -5,22 +5,26 @@ const listItems = document.querySelectorAll('nav ul li')
 
 // loop through each item in the lost of 4 icons with item and it's index
 listItems.forEach((item, idx) => {
+    // Event Listener on icon listening to a click
     item.addEventListener('click', () => {
-        // on lick hide all + add show class in active
+        // #1 - hide all + add show class in active
         hideAllContents()
+        // #2 - hide all items
         hideAllItems()
-        // add class active on the clicked item
+        // #3 make class active on the clicked item
         item.classList.add('active')
-        // as per index, add show class
+        // #4 add show as per index from the for each
         contents[idx].classList.add('show')
     })
 })
 
 function hideAllContents() {
+    // loop through all the image and remove the class show
     contents.forEach(content => content.classList.remove('show'))
 }
 
 
 function hideAllItems() {
+    // loop through all the icon and remove the class active
     listItems.forEach(item => item.classList.remove('active'))
 }
