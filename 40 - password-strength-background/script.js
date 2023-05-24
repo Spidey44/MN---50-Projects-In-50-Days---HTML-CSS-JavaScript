8e8d4b2461158d7561a8db2b02e76097b47b75f1
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-23 23:40:20 -0400

    40ended

--- 40 - password-strength-background/script.js
+++ 40 - password-strength-background/script.js
@@ -1,13 +1,19 @@
+// password input
 const password = document.getElementById('password')
+// background contain image
 const background = document.getElementById('background')
-
+// EL on password input -> fire off on any input
 password.addEventListener('input', (e) => {
+  // -> 1- get the input value in new const
   const input = e.target.value
   console.log('show typing', input)
+  // -> 2- get the length  in new const
   const length = input.length
   console.log('show length', length)
+  // -> 3- Calculate new blur value decremented 2 for each new charin new const
+  // decrease blur decrement -2 for each char -> 18, 16, 14, 12, 
   const blurValue = 20 - length * 2
   console.log('calculation remove blur', 20 - length)
-  // decrease blurr increment by 2 -> 18, 16, 14, 12, 10...0
+  // -> 4- replace the old 20 value by newest  blur value
   background.style.filter = `blur(${blurValue}px)`
 })
