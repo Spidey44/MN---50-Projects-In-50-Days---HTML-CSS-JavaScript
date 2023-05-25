605b35db1c74592610a575a6e677ce139929da28
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-25 08:40:55 -0400

    41done

--- 41 - 3d-boxes-background/script.js
+++ 41 - 3d-boxes-background/script.js
@@ -9,9 +9,9 @@ function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const box = document.createElement('div')
-      console.log("16 boxes", box)
       box.classList.add('box')
       console.log("16 boxes with class", box)
+      // background position has x value ( with J) and y property ( with I)
       // add positioning to each box. All negative values. Onx and Y
       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
       boxesContainer.appendChild(box)
@@ -21,7 +21,7 @@ function createBoxes() {
 
 createBoxes()
 
-
-// values bgd position 00-0, -125-0, -250-0, -375-0
+// background position has x and y property
+// values bgd position 0-0, -125-0, -2500, -375-0
 // values bgd position 0--125, -125--125, -250--125, -375--125
 //...
