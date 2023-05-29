5b7a69d366471b9e5272a5bf814b3ed7dfbaebc5
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-05-29 11:12:53 -0400

    43done

--- 43 - live-user-filter/script.js
+++ 43 - live-user-filter/script.js
@@ -1,24 +1,39 @@
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
+// put fetched data
 const listItems = []
 
+// API random user.me to generate random user
+// browser test 1 user https://randomuser.me/api
+// browser test 50 user  https://randomuser.me/api?results=50
 getData()
 
+// event listener as type in input create event that call function filer data
+// e.target.value gives us what is typed
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
 async function getData() {
+    // await to return the promise with data
     const res = await fetch('https://randomuser.me/api?results=50')
 
+    // destructure
     const { results } = await res.json()
+    // API data may come in different form  ( 1 array,object..) or path
+    console.log('API Result', results)
 
     // Clear result
     result.innerHTML = ''
 
+    // Loop through the results
     results.forEach(user => {
+        console.log('user', user)
+        // create a lit item per user
         const li = document.createElement('li')
 
+        // push each li created in the empty listItem array
         listItems.push(li)
 
+        // add the inner htlm to the li initially hard coded
         li.innerHTML = `
             <img src="${user.picture.large}" alt="${user.name.first}">
             <div class="user-info">
@@ -26,14 +41,16 @@ async function getData() {
                 <p>${user.location.city}, ${user.location.country}</p>
             </div>
         `
-
+        // aff li to UI
         result.appendChild(li)
     })
 }
 
 function filterData(searchTerm) {
+    console.log('term searched', searchTerm)
     listItems.forEach(item => {
-        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+        // convert to check if matches item and remove add class otherwise add
+        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
             item.classList.remove('hide')
         } else {
             item.classList.add('hide')
