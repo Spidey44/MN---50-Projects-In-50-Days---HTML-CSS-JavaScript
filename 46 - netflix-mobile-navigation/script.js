5d4500e118a9e913aa9b9e377cc7a7ab433ef3de
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-06-04 20:37:24 -0400

    45 comlpeted

---
+++ 46 - netflix-mobile-navigation/script.js
@@ -0,0 +1,14 @@
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+// node list to loop through
+const nav = document.querySelectorAll('.nav')
+
+open_btn.addEventListener('click', () => {
+    // loop through each node and add class visible
+    nav.forEach(nav_el => nav_el.classList.add('visible'))
+})
+
+close_btn.addEventListener('click', () => {
+    // loop through each node and remove class visible
+    nav.forEach(nav_el => nav_el.classList.remove('visible'))
+})
