233572d5016bb9e3e5665c4c43d21034ff015dc4
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-06-11 23:08:16 -0400

    49ended

--- 49 - random-image-generator/script.js
+++ 49 - random-image-generator/script.js
@@ -1,17 +1,22 @@
 const container = document.querySelector('.container')
+// URL in variable
 const unsplashURL = 'https://source.unsplash.com/random/'
+// # of rows
 const rows = 5
 
-// 3 images per row
+// as long as we have room for images, less than 3 per row
+// -> increment/create image
 for (let i = 0; i < rows * 3; i++) {
     const img = document.createElement('img')
     img.src = `${unsplashURL}${getRandomSize()}`
     container.appendChild(img)
 }
+// get random size = random image
 console.log(getRandomSize())
 function getRandomSize() {
     return `${getRandomNr()}x${getRandomNr()}?city`
 }
+
 console.log(getRandomNr())
 // get random number between 300 and 310
 function getRandomNr() {
